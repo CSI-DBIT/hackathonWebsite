b08f425a48e876ae809789325d0342b9899ba340
VedantJannu-009 <omjannu2002@gmail.com>
2023-09-10 17:35:20 +0530

    landing page and other pages merged

--- temp1/src/App.js
+++ temp1/src/App.js
@@ -1,19 +1,21 @@
 import Accordion from "./components/Accordian";
-import Card from "./components/card";
+import Card from "./components/Card";
+import LandingPage from "./components/LandingPage";
 import Sponsors from "./components/Sponsors";
 
 function App() {
   return (
     <>
+      <LandingPage/>
       {/* updated card */}
-      <Card></Card> 
+      <Card></Card>
       <Sponsors></Sponsors>
       <div className='accordion-header'>PROBLEM STATEMENT</div>
       <div>
-      <Accordion title="FINTECH" content="Lorem ipsum dolor sit amet." />
-      <Accordion title="SUSTAINAIBILITY" content="Consectetur adipiscing elit." />
-      <Accordion title="EDUCATION" content="Sed do eiusmod tempor incididunt." />
-    </div>
+        <Accordion title="FINTECH" content="Lorem ipsum dolor sit amet." />
+        <Accordion title="SUSTAINAIBILITY" content="Consectetur adipiscing elit." />
+        <Accordion title="EDUCATION" content="Sed do eiusmod tempor incididunt." />
+      </div>
     </>
   );
 }
---
+++ temp1/src/components/LandingPage.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import "../css/landingPage.css";
+const LandingPage = () => {
+    return (
+        <>
+            <div class="clock-wrapper">
+                <div class="clock">
+                    <div class="clock__inner-bg"></div>
+                    <div class="clock__inner-bg">
+                        <div class="clock__markers">
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                            <div class="clock__mark"></div>
+                        </div>
+                    </div>
+                    <div class="clock__inners">
+                        <div class="clock__inner-center">
+                            <div class="clock__markers">
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                                <div class="clock__mark"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="titleScreen">
+                <div className="a">
+                    <p>CSI DBIT PRESENTS</p>
+                </div>
+                <div className="b">
+                    <p>CODEVERSE</p>
+                </div>
+                <div className="c">
+                    <p># CODE FOR CAUSE</p>
+                </div>
+            </div>
+            </>
+    )
+}
+
+export default LandingPage;
